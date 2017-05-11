c3f1179518306a28e1625cf4afdff315f2facfa3
Msmo15 <exxri@hotmail.com>
2017-05-11 11:14:22 +0200

    Add project files.

---
+++ HelpfulFriendMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelpfulFriendMVC.MvcApplication" Language="C#" %>
---
+++ HelpfulFriendMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelpfulFriendMVC.Startup))]
+namespace HelpfulFriendMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
