f6c91279a076f66f94ae3b9aa4c0773a57317825
Msmo15 <exxri@hotmail.com>
2017-05-14 21:49:28 +0200

    Database setup

---
+++ HelpfulFriendMVC/Controllers/JobsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HelpfulFriendMVC.Models;
+
+namespace HelpfulFriendMVC.Controllers
+{
+    public class JobsController : Controller
+    {
+        private HelpfulFriendDatabaseEntities db = new HelpfulFriendDatabaseEntities();
+
+        // GET: Jobs
+        public ActionResult Index()
+        {
+            var jobs = db.Jobs.Include(j => j.Category1);
+            return View(jobs.ToList());
+        }
+
+        // GET: Jobs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = db.Jobs.Find(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job);
+        }
+
+        // GET: Jobs/Create
+        public ActionResult Create()
+        {
+            ViewBag.Category = new SelectList(db.Categories, "Category1", "Category1");
+            return View();
+        }
+
+        // POST: Jobs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "JobID,JobTitle,Category,Address,JobDate,Price")] Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Jobs.Add(job);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Category = new SelectList(db.Categories, "Category1", "Category1", job.Category);
+            return View(job);
+        }
+
+        // GET: Jobs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = db.Jobs.Find(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Category = new SelectList(db.Categories, "Category1", "Category1", job.Category);
+            return View(job);
+        }
+
+        // POST: Jobs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "JobID,JobTitle,Category,Address,JobDate,Price")] Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(job).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Category = new SelectList(db.Categories, "Category1", "Category1", job.Category);
+            return View(job);
+        }
+
+        // GET: Jobs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = db.Jobs.Find(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job);
+        }
+
+        // POST: Jobs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Job job = db.Jobs.Find(id);
+            db.Jobs.Remove(job);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
